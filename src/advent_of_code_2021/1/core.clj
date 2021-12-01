2e4bfa2855e01373c95787ec786ae01408b13258
Frantisek Kobzik <hustodemon@users.noreply.github.com>
2021-12-01 19:48:38 +0100

    1, part 2

--- src/advent_of_code_2021/1/core.clj
+++ src/advent_of_code_2021/1/core.clj
@@ -14,5 +14,16 @@
     (< fst snd) (recur (inc cnt) (rest all))
     :else       (recur cnt       (rest all))))
 
+(prn "Part 1: " (count-increasing 0 data))
 
-(count-increasing 0 data)
+
+(defn count-increasing-2 [cnt [_ & rst :as ms]]
+  (let [win-1 (take 3 ms)
+        win-2 (take 3 rst)]
+    (cond
+      (not= 3 (count win-1) (count win-2))  cnt
+      (< (apply + win-1) (apply + win-2))   (recur (inc cnt) rst)
+      :else                                 (recur cnt rst))))
+
+(prn "Part 2: " (count-increasing-2 0 data))
+
