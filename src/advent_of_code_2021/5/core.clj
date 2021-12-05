95e47aaecd5134e07ea1edfc8fb5faf7c001cdb5
Frantisek Kobzik <hustodemon@users.noreply.github.com>
2021-12-05 12:55:37 +0100

    Green deal-friendly alternative of part 2

--- src/advent_of_code_2021/5/core.clj
+++ src/advent_of_code_2021/5/core.clj
@@ -26,53 +26,67 @@
     (range smaller (inc bigger))))
 
 
+(defn- is-straight?
+  "True if given pair of coords are a horizontal or vertical line."
+  [[[x1 y1] [x2 y2] :as line-coord]]
+  (or (= x1 x2) (= y1 y2)))
+
+
 (defn- compute-line-points [[[x1 y1] [x2 y2] :as line-coord]]
   [x1 y1 x2 y2]
-  (if (or (= x1 x2) (= y1 y2)) ;; only consider lines with at least one common coord
+  (if (is-straight? line-coord)
     (for [x (numbers-between x1 x2)
           y (numbers-between y1 y2)]
       [x y])
     []))
 
 
-(defn compute-1 [data]
-  (->> data
-       (mapcat compute-line-points)
+(defn- compute
+  "Compute the count of overlapping points.
+  Takes lines and a line-points-fn (a function generating points
+  corresponding to a line between 2 points)."
+  [lines line-points-fn]
+  (->> lines
+       (mapcat line-points-fn)
        (frequencies)
        (vals)
        (filter #(>= % 2))
        (count)))
+
+(defn compute-1 [data] (compute data compute-line-points))
 
 
 (println "Part 1: " (compute-1 data))
 
 
+;; Part 2
 (defn- is-diagonal? [[[x1 y1] [x2 y2] :as line-coord]]
   (= (Math/abs (- x1 x2))
      (Math/abs (- y1 y2))))
 
 
-(defn- compute-line-points-2 [[[x1 y1] [x2 y2] :as line-coord]]
-  [x1 y1 x2 y2]
-  (cond
-    ;; horizontal/vertical
-    (or (= x1 x2) (= y1 y2))  (for [x (numbers-between x1 x2)
-                                    y (numbers-between y1 y2)]
-                                [x y])
-    ;; diagonal
-    (is-diagonal? line-coord) (for [x (numbers-between x1 x2)
-                                    y (numbers-between y1 y2)
-                                    :when (is-diagonal? [[x y] [x1 y1]])]
-                                [x y])
-    :else                     []))
+(defn- walk-closer
+  "Given 2 points (src and dst), compute the next point on the path from src to dst.
+  If src == dst, return the result wrappped in `reduced`"
+  [[[x1 y1 :as src] [x2 y2 :as dst] :as line-coord]]
+  [(cond
+     (< x1 x2) (inc x1)
+     (> x1 x2) (dec x1)
+     :else     x1) ;; egal
+   (cond
+     (< y1 y2) (inc y1)
+     (> y1 y2) (dec y1)
+     :else     y1)])
 
 
-(defn compute-2 [data]
-  (->> data
-       (mapcat compute-line-points-2)
-       (frequencies)
-       (vals)
-       (filter #(>= % 2))
-       (count)))
+(defn- compute-line-points-2 [[[x1 y1 :as src] [x2 y2 :as dst] :as line-coord]]
+  ;; make sure we are dealing with either horizontal, vertical, or diagonal
+  ;; lines, otherwise walking will be not be straight
+  (if (= src dst) [src]
+      (conj (compute-line-points-2 [(walk-closer line-coord) dst]) src)))
+
+
+(defn compute-2 [data] (compute data compute-line-points-2))
+
 
 (println "Part 2: " (time (compute-2 data)))
